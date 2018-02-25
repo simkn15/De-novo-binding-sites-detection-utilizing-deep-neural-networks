798b844a0c25feb643bc30044378614813c8ebc2
simkn15 <simkn15@student.sdu.dk>
2018-02-25 10:24:21 +0100

    Learning the basics of Keras and how to implement Autoencoder with Keras.

---
+++ autoencoder/autoencoder.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.datasets import mnist
+
+# load data
+
+# Create encoding layers:
+# - Input layer
+# - Hidden layers
+
+# Create decoding layers:
+# - One or multiple ?
+
+# Train our autoencoder
+'''
+Decide on:
+- Optimizer (Is this the learning rate ? (Stochastic Gradient Descent?)) : simpleAutoencoder.py = adadelta
+- Loss function : simpleAutoencoder.py = binary_crossentropy
+
+
+'''
